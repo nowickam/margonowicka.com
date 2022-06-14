b16ff4eae7c740e7cdb18aa19ab2fad3dfe5ed23
Nowicka Malgorzata <malgorzata.nowicka@aalto.fi>
2022-06-14 10:47:19 +0300

    dysmorphia & invisible edit

--- src/work/data.js
+++ src/work/data.js
@@ -1,14 +1,49 @@
 export const data = [
     {
+        title: "Dysmorphia",
+        img: "",
+        text: [
+            "Dysmorphia is a term related to a body-image disorder, where one believes a part of their appearance is severely distorted or flawed, in contrary to reality. In this work I morph the deformed perception of the body into a deformed perception of the mind, a mental dysmorphia. An expression of one’s self warping and deforming in the presence of others.",
+
+            "As one goes about their day, interacts with other people, the need for an ‘I’ arises. A symbol of one's image, summary of one's existence, place in the social dynamics, a colour to the contents of one's mind.",
+
+            "This phenomenon generates worry and stress, it feels like a contraction of consciousness, attention feedback loop like that of a painful organ (one does not acknowledge the existence of one's pancreas until it starts hurting). Self-conscious thinking patterns start poisoning every interaction one has, and the grasp on reality gets weaker and weaker. ",
+
+            "Then, the ‘I’ starts being observed. ‘I’ starts being examined. ‘I’ starts internalising the outside observer, in the need of control. What do you think of me? How do you see me? Can I predict what image of me you have? Simone de Beauvoir said: “when a girl becomes a woman she is doubled; instead of coinciding exactly with herself she also exists outside”. Watching herself being watched.",
+
+            "The feeling of the ‘I’ and being in control of its image - all of it is an illusion. A painful one, since the need of being in control can never be satisfied. The tendency to assume the worst leads to self-detrimental thoughts. Confrontation with the distorted image one has, leads into a squishing awkwardness, painful assignment of flaws, division of the self, indecisiveness. ",
+
+            "Sometimes, one is able to cool down the disrupting thoughts and sensations, recover from the overwhelming realisations. Sometimes, the interaction generates reactions which one only exaggerates, spiralling into the egocentric doom. ",
+
+            "One always hopes that it will all change. Once the ‘I’ starts fresh and wakes up to a new day, everything will be alright. Then the ‘I’ can finally become a loving partner, kind friend, engaged citizen. But with time one gets tired and learns that there is no escape to the illusion and the torment it generates. That one needs to reject the notion of the ‘I’ but at the same time reject the need to do it. One goes in a circle, being tossed by the projections of ego, external and internal. Understandably, loosing hope and giving into dysmorphia. The only solution is to coincide with the contents of the consciousness, flow without creating a narrative of it.",
+
+            "Animation: Margo Nowicka \n Sound: Lucien Montandon \n Supervision: Kai Lappalainen \n"
+        ],
+        links: {
+            "too see the animation, please reach out by email": "mailto:mngnowicka@gmail.com"
+        },
+        mainMedia: {
+            "img1": "/media/dys1.jpg",
+        },
+        media: {
+            "img1": "/media/dys2.jpg",
+            "img2": "/media/dys3.jpg",
+            "img3": "/media/dys6.jpg",
+            "img4": "/media/dys5.jpg",
+            // "img5": "/media/dys6.jpg",
+        },
+        description: "3d animation"
+    },
+    {
         title: "Invisible Lines",
         img: "/thumbnails/ada.png",
         text: [
             "The work comprises of two large projections facing each other. On the one side the colors of the images of waves, forest, underwater reef are bleeding into the silhouette of the person entering the space. The feeling of calm is reflected in the sounds of wind and birds. The opposing screen shows burning destruction, glitchy horror happening outside of the person’s reflection, with a dissolution of crimson reds and blacks inside it. Sounds of underwater volcanoes and explosions accompany the turmoil.",
             "When one travels between the two extremes, one notices an invisible border in the middle, that unveils where one arrives, and blurs the place that is left behind.",
-            "The structure resembles the one of triptych, contraposing two states of the internal, emotional and external, environmental, with the viewer in the middle, exerting their agency over the order and the chaos, Eden and Last Judgement.",
-            "What is so appealing about this simple form is that it escapes the false subtleties and elaborated explanations of the current-day manipulators, and portrays a cry that has stayed unchanged for centuries. Why do I deserve to live and you don’t? A childish cry - one might say it’s politics, history, money. In the face of people fleeing their homes, being tortured and dead, this explanation still doesn’t make sense.",
-            "What makes us different from the earthly characters of Bosh’ imagination is that we can see what happens to the left and to the right, behind and in front of the central panel. Sheltered on one side, we can see the bombs detonating, feel the earth trembling from its impact. Running from the horrors of hell, you’re safe on the other side of the imaginary line.",
-            "Borges warns of imaginary, constructed worlds, and warns of triptychs.",
+            "The structure resembles the one of triptych, contraposing two states of the internal, emotional and external, environmental, with the viewer in the middle of ‘The Garden of Earthly Delights’, exerting their agency over the order and the chaos, Eden and Last Judgement.",
+            "What makes us different from the earthly characters of Bosh’ imagination is that we can see what happens to the left and to the right, behind and in front of the central panel. Sheltered on one side, we can see the bombs detonating, feel the earth trembling from its impact. Running from the horrors of hell, we are safe on the other side of the imaginary line.",
+            "This simple form of a triptych escapes the false subtleties and elaborated explanations of the current-day manipulators, and portrays a cry that has stayed unchanged for centuries. Why do I deserve to live and you don’t? Do I have any agency? Childish cries - one might say it is about politics, history, money. Watching people fleeing their homes, being tortured and dead - in the face of it all this explanation still doesn’t make sense.",
+            "Borges warns of imaginary, constructed worlds, warns of the emergent triptychs.",
             "‘Ten years ago any symmetry with a semblance of order - dialectical materialism, anti-Semitism, Nazism - was sufficient to entrance the minds of men. How could one do other than submit to [the constructed symmetry], to the minute and vast evidence of an orderly planet? It is useless to answer that reality is also orderly. Perhaps it is, but in accord­ance with divine laws - I translate: inhuman laws - which we never quite grasp. The contact and the habit of [the constructed, symmetrical planet] have disintegrated this world. Enchanted by its rigor, humanity forgets over and again that it is a rigor of chess masters, not of angels.’",
             "Rigorous drawing of invisible lines disturbs the inhuman laws of this world, that we so eagerly want to make our own. At the same time, the very human nature of our constructs makes them paradoxically pointless - why did we agree that this invisible line goes here, what is the reason for creating hells and heavens of this world, side by side.",
             "The construction of fake dualism, idealistic and absurd, serves the chess masters playing within their squares of the divine board ignoring the suffering of everything that happens to appear on it.",
@@ -19,13 +54,15 @@ export const data = [
             "video": "https://youtu.be/LiDLAK-ukv8",
         },
         mainMedia: {
-            "video": "/media/lines.mp4",
+            "video": "/media/lines1.mp4",
         },
         media: {
-            "img1": "/media/lines1.png",
+            "video1": "/media/lines.mp4",
             "img2": "/media/lines2.png",
-            "video1": "/media/lines1.mp4",
-            "video2": "/media/lines2.mp4"
+            "img1": "/media/lines1.png",
+
+
+            // "video2": "/media/lines2.mp4"
         },
         description: "interactive installation, touchdesigner"
 
@@ -82,6 +119,8 @@ export const data = [
             "It was a long time ago, when my mum who told me that the seashells remember the ocean. With a childish response, I listened and listened to the hum of the moving water, crushing of the waves enclosed in this cavity. Mum's protective narrative bubble burst, unveiling a world that wants me to enjoy its chaos and mourn its entropy.",
             "It was a month ago that I finally learned what the noise in the seashell actually is. Stripped of another myth that it is the sound of blood coursing through my body, I found out that the shell resonates the surrounding ambient sound. Having rendered my own pulse onto the world, I realize it is not about me, I was deceived by my own bias",
             "It is now that I am frantically putting myself back in the shell. Shielding myself from anxiety and fear springing from the interaction with the cacophony of yells and cries. I don't speak, laugh, respond to texts, invite to my home, I bury myself in the depths of the shell and slowly fossilize to become a part of it.",
+            "Repeating after Fernando Pessoa:",
+            "‘Believing that each step of my life would mean contact with the horror of the New and that each new person I met was a new and living fragment of the unknown to be placed before me on the table for my daily horrified contemplation, I decided to abstain from everything, to go nowhere, to reduce action to the minimum, to avoid as far as possible meeting either men or events, to perfect abstinence and take abdication to new heights. That's how much living frightens and torments me.’",
             "This project comprises of two seashells that one can listen to. The first seashell synthesizes underwater ocean ambient sounds depending on the shell’s tilt. The second seashell streams several distorted radio channels. So the two seashells contrapose each other in a fight for the imprint in the collective memory; and at the same time represent a progression, going outwards, from tranquility to overwhelm."
         ],
         links: {
